f9a66c57eea42c7c190df9e076ea2a15fe0ce28a
Raghavendra Gautam <raghavendragautam@live.com>
2023-12-21 12:04:35 +0530

    cosmetic changes

--- bucket.tf
+++ bucket.tf
@@ -7,20 +7,6 @@
 #   public_access_prevention = "enforced"
 # }
 
-# data "google_iam_policy" "admin" {
-#   binding {
-#     role = "roles/storage.admin"
-#     members = [
-#       "user:jane@example.com",
-#     ]
-#   }
-# }
-
-# resource "google_storage_bucket_iam_policy" "policy" {
-#   bucket = google_storage_bucket.default.name
-#   policy_data = data.google_iam_policy.admin.policy_data
-# }
-
 
 # output "bucket_name" {
 #   value = resource.google_storage_bucket.fireflies-ai-gcp-byos-bucket.id
